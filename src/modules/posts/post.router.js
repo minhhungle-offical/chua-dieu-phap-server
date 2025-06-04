3eab99180285f708c8d5086c763774b9b13b8361
minhhungle <minhhungle.offical@gmail.com>
2025-06-04 13:45:17 +0700

    update

--- src/modules/posts/post.router.js
+++ src/modules/posts/post.router.js
@@ -1,19 +1,39 @@
 import express from 'express'
 import { authMiddleware } from '../../middleware/auth.middleware.js'
 import { authorize } from '../../middleware/authorize.middleware.js'
-import { createPost, deletePost, getPostById, getPosts, updatePost } from './post.controller.js'
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  getPostBySlug,
+  getPosts,
+  updatePost,
+} from './post.controller.js'
 import { singleUpload } from '../../middleware/upload.js'
 import { handleMulterError } from '../../middleware/handleMulterError.js'
 
 const postRouter = express.Router()
 
 postRouter.get('/', getPosts)
+postRouter.get('/slug/:slug', getPostBySlug)
 postRouter.get('/:id', getPostById)
 
-postRouter.use(authMiddleware)
-
-postRouter.post('/', authorize(['admin', 'staff']), singleUpload, handleMulterError, createPost)
-postRouter.put('/:id', authorize(['admin', 'staff']), singleUpload, handleMulterError, updatePost)
-postRouter.delete('/:id', authorize(['admin', 'staff']), deletePost)
+postRouter.post(
+  '/',
+  authMiddleware,
+  authorize(['admin', 'staff']),
+  singleUpload,
+  handleMulterError,
+  createPost,
+)
+postRouter.put(
+  '/:id',
+  authMiddleware,
+  authorize(['admin', 'staff']),
+  singleUpload,
+  handleMulterError,
+  updatePost,
+)
+postRouter.delete('/:id', authMiddleware, authorize(['admin', 'staff']), deletePost)
 
 export default postRouter
